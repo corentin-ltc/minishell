b38956015de375e1c2eda1bc8065bd83fac1f6ed
Corentin Le Tortorec <cle-tort@paul-f5Ar2s4.clusters.42paris.fr>
2024-08-15 19:01:04 +0200

    AAAAH

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <unistd.h>
 
 int main(int argc, char **argv, char **env)
 {
 	char *line;
+	char *prompt;
+	char *userline;
+	int i;
 
-	line = readline("minishelll > ");
-	printf("%s", line);
+	userline = NULL;
+	i = 0;
+	while (env[++i])
+	{
+		if (ft_strncmp(env[i], "USER=", 5) == 0)
+		{
+			userline = ft_strdup(env[i] + 5);
+			break ;
+		}
+	}
+	printf("%s", userline);
 }
