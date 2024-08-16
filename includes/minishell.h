e9a40311fc3cb19c0009142edb1784d4ca21428c
Nabil080 <nbellila@student.42.fr>
2024-08-16 16:34:59 +0200

    include signal.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,17 +6,19 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 16:05:54 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/16 16:09:23 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/08/16 16:29:40 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#	define MINISHELL_H
+# define MINISHELL_H
 
 /*libft*/
 # include "libft.h"
 /*readline*/
 # include <readline/readline.h>
 # include <readline/history.h>
+/*signals*/
+# include <signal.h>
 
 #endif
