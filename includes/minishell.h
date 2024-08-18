8b64860995d32134a34c2769e9ef3b705f37a068
Nabil080 <nbellila@student.42.fr>
2024-08-18 15:10:29 +0200

    init.c & minishell_loop, data struct initialization

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,25 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 16:05:54 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/18 14:11:57 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/08/18 15:10:01 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/*structs*/
+typedef struct s_cmd{
+	char	**args;
+	int		in_fd;
+	int		out_fd;
+	int		is_heredoc;
+}t_cmd;
+typedef struct s_data{
+	char	**env;
+	char	*line;
+	t_cmd	**cmds;
+}t_data;
 /*libft*/
 # include "libft.h"
 /*readline*/
@@ -24,7 +36,7 @@
 # include <sys/types.h>
 # include <dirent.h>
 /*others*/
-# include "signals.h"
+# include "init.h"
 # include "builtin.h"
 # include "errors.h"
 # include "utils.h"
--- srcs/init/init.c
+++ srcs/init/init.c
@@ -1,36 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   errors.c                                           :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/17 17:46:40 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/18 15:06:00 by nbellila         ###   ########.fr       */
+/*   Created: 2024/08/18 14:59:26 by nbellila          #+#    #+#             */
+/*   Updated: 2024/08/18 15:10:02 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	shell_error(char *cmd, char *message)
+void	init_data(t_data *data, char **env)
 {
-	ft_putstr_fd(cmd, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putendl_fd(message, 2);
+	data->env = ft_arrdup(env);
+	if (!data->env)
+		exit_error("An allocation failed", NULL);
+	data->line = NULL;
 }
 
-void	exit_error(char *str, t_data *data)
+void	minishell_loop(t_data *data)
 {
-	ft_putstr_fd(str, 2);
-	if (data)
-		free_data(data);
-	exit(EXIT_FAILURE);
-}
-
-void	free_data(t_data *data)
-{
-	if (data->env)
-		free_2d((void **)data->env, 0);
-	if (data->line)
+	while (true)
+	{
+		data->line = readline("minishell > ");
+		if (!data->line)
+			exit_error("success", data);
+		add_history(data->line);
+		// check_builtin(ft_split(data->line, " "), &data->env);
 		free(data->line);
+	}
 }
