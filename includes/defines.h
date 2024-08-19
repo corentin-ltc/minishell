e8e6262c8ed34cd9e4c0a5dcccb17786ab960dc8
Nabil080 <nbellila@student.42.fr>
2024-08-19 18:48:00 +0200

    split by set instead of char

--- includes/defines.h
+++ includes/defines.h
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 15:51:20 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/18 21:10:00 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/08/19 18:41:17 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,16 @@ typedef struct s_data{
 	char	**env;
 	char	*line;
 	t_cmd	**cmds;
+	char	**tokens;
 }t_data;
 
 typedef struct s_parser{
 	bool	d_quotes;
 	bool	s_quotes;
-	char	sep;
+	bool	infile;
+	bool	here_doc;
+	bool	outfile;
+	char	*set;
 }t_parser;
 
 #endif
