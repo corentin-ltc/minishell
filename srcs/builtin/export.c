2a25ce40c75001763c0854806509bbffb06b8ded
Nabil080 <bellilanabil8@gmail.com>
2024-08-22 22:47:01 +0200

    export & unset correct exit code

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 18:28:07 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/22 21:10:37 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/08/22 22:45:28 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_export(t_data *data, t_cmd *cmd)
 	size_t	i;
 	size_t	name;
 
+	data->exit_code = 0;
 	i = 1;
 	while (cmd->args[i])
 	{
@@ -27,8 +28,10 @@ void	ft_export(t_data *data, t_cmd *cmd)
 		{
 			cmd->args[i][name] = '\0';
 			if (!ft_setenv(cmd->args[i], &(cmd->args[i][name + 1]), &(data->env)))
-				exit(EXIT_FAILURE);
+				exit_error("setenv alloc failed", data);
 		}
+		else
+			data->exit_code = 1;
 		i++;
 	}
 }
