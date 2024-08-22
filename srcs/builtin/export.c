c63fee6be0b84e9746427f99fb45953da9255961
Nabil080 <nbellila@student.42.fr>
2024-08-22 20:42:18 +0200

    fix export when simple name without equal sign

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 18:28:07 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/22 20:37:20 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/08/22 20:41:26 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 void	ft_export(t_data *data, t_cmd *cmd)
 {
 	size_t	i;
-	size_t	len;
+	size_t	name;
 
 	i = 1;
 	while (cmd->args[i])
 	{
-		len = 0;
-		while (cmd->args[i][len] && cmd->args[i][len] != '=')
-			len++;
-		cmd->args[i][len] = '\0';
-		if (len != 0 || len != ft_strlen(cmd->args[i]))
+		name = 0;
+		while (cmd->args[i][name] && cmd->args[i][name] != '=')
+			name++;
+		if (name > 0 && cmd->args[i][name] == '=')
 		{
-			if (!ft_setenv(cmd->args[i], &(cmd->args[i][len + 1]), &(data->env)))
+			cmd->args[i][name] = '\0';
+			if (!ft_setenv(cmd->args[i], &(cmd->args[i][name + 1]), &(data->env)))
 				exit(EXIT_FAILURE);
 		}
 		i++;
