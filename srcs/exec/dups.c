272456417934d7d5615f68ac48c7631b361aec9e
Nabil080 <nbellila@student.42.fr>
2024-08-22 21:10:52 +0200

    single builtin correct redirections

--- srcs/exec/dups.c
+++ srcs/exec/dups.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 19:47:42 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/21 23:03:58 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/08/22 21:10:37 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ static void	dup_infile(t_data *data, t_cmd *cmd, size_t index)
 {
 	// first cmd : dup infile soit STDIN soit in_fd
 	if (index == 0 && cmd->in_fd)
-		dup2(data->cmds[0]->in_fd, STDIN_FILENO);
+		dup2(cmd->in_fd, STDIN_FILENO);
 	// autre cmd : dup infile soit PIPE soit in_fd
 	else if (index != 0)
 	{
 	}
-	if (data->cmds[0]->in_fd > 0)
-		close(data->cmds[0]->in_fd);
+	if (cmd->in_fd > 0)
+		close(cmd->in_fd);
 }
 
 static void	dup_outfile(t_data *data, t_cmd *cmd, size_t index)
@@ -30,7 +30,7 @@ static void	dup_outfile(t_data *data, t_cmd *cmd, size_t index)
 	// last cmd : dup outfile soit STDOUT soit out_fd
 	if (data->cmds[index + 1] == NULL && cmd->out_fd)
 		dup2(cmd->out_fd, STDOUT_FILENO);
-	// autres : dup outfile soit PIPE soit out_fd
+	// autres : dup outfile soit	 PIPE soit out_fd
 	else if (data->cmds[index + 1])
 	{
 	}
