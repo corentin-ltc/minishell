6ababa997abb54b4b34eea09fb248269f9ecfaee
Nabil080 <bellilanabil8@gmail.com>
2024-08-30 18:27:52 +0200

    fix infile et outfile open en meme temps

--- srcs/parsing/files.c
+++ srcs/parsing/files.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   outfiles.c                                         :+:      :+:    :+:   */
+/*   infiles.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/20 00:20:30 by nbellila          #+#    #+#             */
-/*   Updated: 2024/08/28 01:51:05 by nbellila         ###   ########.fr       */
+/*   Created: 2024/08/28 00:26:21 by nbellila          #+#    #+#             */
+/*   Updated: 2024/08/30 18:25:18 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,19 +61,74 @@ static void	handle_outfile(t_data *data, t_cmd *cmd, t_parser *pars, size_t *i)
 	cmd->line = new_line;
 }
 
-void	get_outfile(t_data *data, t_cmd *cmd)
+static void	update_infile(t_data *data, t_cmd *cmd, t_parser *pars, char *name)
+{
+	char	*trimmed;
+
+	trimmed = ft_strtrim(name, " ");
+	if (!trimmed)
+		exit_error("malloc", data);
+	if (cmd->in_fd > 0)
+		close(cmd->in_fd);
+	if (pars->here_doc == true)
+		cmd->in_fd = open(cmd->heredoc, O_RDONLY, 0777);
+	else
+		cmd->in_fd = open(trimmed, O_RDONLY, 0777);
+	if (cmd->is_valid && cmd->in_fd < 0)
+	{
+		perror(trimmed);
+		cmd->is_valid = false;
+	}
+	free(trimmed);
+}
+
+static void	handle_infile(t_data *data, t_cmd *cmd, t_parser *pars, size_t *i)
+{
+	char	*new_line;
+	char	*name;
+	size_t	start;
+
+	start = *i;
+	while (ft_isspace(cmd->line[*i]))
+		parse_str(pars, cmd->line, i);
+	if (!cmd->line[*i])
+		exit_error("syntax error near unexpected token '<'", data);
+	while (pars->quotes || !ft_isfile_limiter(cmd->line[*i]))
+		parse_str(pars, cmd->line, i);
+	name = get_filename(cmd->line + start, *pars);
+	if (!name)
+		exit_error("malloc", data);
+	update_infile(data, cmd, pars, name);
+	free(name);
+	start--;
+	if (pars->here_doc)
+		start--;
+	new_line = ft_strcut(cmd->line, start, *i - 1);
+	if (!new_line)
+		exit_error("malloc", data);
+	free(cmd->line);
+	cmd->line = new_line;
+}
+
+void	get_files(t_data *data, t_cmd *cmd)
 {
 	t_parser	parser;
 	size_t		i;
 
 	parser = new_parser();
 	i = 0;
-	while (parse_str(&parser, cmd->line, &i))
+	while (cmd->is_valid && parse_str(&parser, cmd->line, &i))
 	{
+		if (parser.infile)
+		{
+			handle_infile(data, cmd, &parser, &i);
+			parser = new_parser();
+			i = 0;
+		}
 		if (parser.outfile)
 		{
 			handle_outfile(data, cmd, &parser, &i);
-			parser.outfile = false;
+			parser = new_parser();
 			i = 0;
 		}
 	}
